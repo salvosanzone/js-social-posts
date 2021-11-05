fda239680a45bef2ad9d4ab5e7904958f468e8e7
salvosanzone <salvosansone.ss@gmail.com>
2021-11-05 15:09:26 +0100

    inserimento degli oggetti nel template

--- main.js
+++ main.js
@@ -86,56 +86,56 @@ for(let i = 0; i < posts.length; i++){
     //a questo div do una classe gia creata in css
     post.className = 'posts-list';
     //tramite un template inserisco tutti gli oggetti presenti nell'array usando innerHTML
-    container.innerHTML = `
+    post.innerHTML = `
     
     <div class="post">
             <div class="post__header">
                 <div class="post-meta">                    
                     <div class="post-meta__icon">
-                        <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                        <img class="profile-pic" src="${posts[i].media}"alt="Phil Mangione">                    
                     </div>
                     <div class="post-meta__data">
-                        <div class="post-meta__author">Phil Mangione</div>
-                        <div class="post-meta__time">4 mesi fa</div>
+                        <div class="post-meta__author">${posts[i].name}</div>
+                        <div class="post-meta__time">${posts[i].created}</div>
                     </div>                    
                 </div>
             </div>
-            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__text">${posts[i].content}.</div>
             <div class="post__image">
                 <img src="https://unsplash.it/600/300?image=171" alt="">
             </div>
             <div class="post__footer">
                 <div class="likes js-likes">
                     <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <a class="like-button  js-like-button"${posts[i].id}>
                             <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                             <span class="like-button__label">Mi Piace</span>
                         </a>
                     </div>
                     <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
                     </div>
                 </div> 
             </div>            
         </div>
     
     
-    `
+    `;
 
 
 
 
 
 
-
+    //appendo al container il post
+    container.append(post);
 }
 
 
 
-/*
-3. 
-4. stampo
-*/  
+
+
+ 
 
 
 
